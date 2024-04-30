c9a2a3f6df73a3d616123da1b7c6e5f819031816
bablysingh2409 <97339369+bablysingh2409@users.noreply.github.com>
2024-04-30 17:48:13 +0530

    added footer and new arrivals

--- components/NewArrival.jsx
+++ components/NewArrival.jsx
@@ -13,16 +13,18 @@ function NewArrival() {
   return (
     <div className="mt-6 overflow-x-auto whitespace-nowrap hide-scrollbar">
       <h1 className="px-2 py-1 text-2xl font-semibold">New Arrivals</h1>
-      <div className="w-full flex overflow-x-scroll gap-2 items-center mt-2 hide-scrollbar">
+      <div className="w-full flex overflow-x-scroll items-center mt-2 hide-scrollbar gap-2 ">
         {data.map((item, i) => {
           return (
-            <div key={i} className="flex justify-center items-end flex-col">
-              <div className="">
-                <img src="/newArrivalShoe.png" alt="shoe" />
+            <div key={i} className="flex justify-center items-center flex-col shadow-lg p-4 " style={{ minWidth: "350px" }}>
+              <div className="w-64 flex justify-center items-center">
+                <img src="/newArrivalShoe.png" alt="shoe" 
+                    className="w-3/4"
+                />
               </div>
-              <div>
-                <p>{item.name}</p>
-                <p>{item.price}</p>
+              <div className="w-48 flex justify-center items-center flex-col py-2 ">
+                <p className="uppercase text-xl font-semibold">{item.name}</p>
+                <p className="text-sm font-semibold text-gray-600">{item.price}</p>
               </div>
             </div>
           );
